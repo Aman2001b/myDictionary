f1722a9cc2e53a27a2aa449f703a0a02ef50bc4a
Aman <aman2001b@gmail.com>
2022-10-27 20:01:01 +0530

    Initial commit

---
+++ src/main/java/com/example/mydictionary/MyDictionary.java
@@ -0,0 +1,107 @@
+package com.example.mydictionary;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MyDictionary extends Application {
+    private Group tileGroup = new Group();
+
+    int xLine = 20;
+    int yLine = 20;
+    int yLine2 = 50;
+    DictionaryUsingHashMap dictionary;
+
+    private Pane createContent(){
+        Pane root = new Pane();
+        root.setPrefSize(400,400);
+        root.getChildren().addAll(tileGroup);
+
+        dictionary = new DictionaryUsingHashMap();
+
+        TextField wordText = new TextField("Word");
+        wordText.setTranslateX(xLine);
+        wordText.setTranslateY(yLine);
+
+        Label meaningLabel = new Label("I am meaning");
+        meaningLabel.setTranslateX(xLine);
+        meaningLabel.setTranslateY(yLine2);
+
+
+
+
+        Button searchButton = new Button("Search");
+        searchButton.setTranslateX(xLine+200);
+        searchButton.setTranslateY(yLine);
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String word = wordText.getText();
+                if(word.isBlank()) {
+                    meaningLabel.setText("Please enter a word");
+                }
+                else {
+                    meaningLabel.setText(dictionary.findMeaning(word));
+                }
+            }
+        });
+
+
+
+
+
+
+
+        tileGroup.getChildren().addAll(wordText,searchButton,meaningLabel);
+        return root;
+    }
+    public void connectToDataBase(){
+        final String DB_URL = "jdbc:mysql://localhost:3306/my_dict";
+        final String USER = "root";
+        final String PASS = "aman2001b";
+
+        System.out.println("Connecting to database");
+        String newId = "select * from word_list";
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(newId);
+        ) {
+            while (rs.next()) {
+                //Display values
+                System.out.println(rs.getInt("id") + rs.getString("word") + rs.getString("meaning")); //rs.getInt("rollno");
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+      //  FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        connectToDataBase();
+        Scene scene = new Scene(createContent());
+        stage.setTitle("My Dictionary !");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
